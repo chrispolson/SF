7123b2dda514020e8c166785cc52fe04cafb2067
chris284 <chris_284@yahoo.com>
2024-05-28 11:48:15 -0400

    refactor our APIClient to have the structure for async/await

--- SquadFormers/SquadFormers/Service/APIService.swift
+++ SquadFormers/SquadFormers/Service/APIService.swift
@@ -1,8 +1,9 @@
 import Foundation
 
-class APIClient {
-    static func fetchArticles(completion: @escaping (Result<[Article], Error>) -> Void) {
+struct APIClient {
+    func fetchArticles() async throws -> [Article] {
         let sampleArticles = [Article(title: "iOS Development"), Article(title: "SwiftUI Essentials")]
-        completion(.success(sampleArticles))
+        
+        return sampleArticles
     }
 }
